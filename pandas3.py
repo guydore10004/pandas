39f00706b0d34821ff9137161ccf1cdd83aeb524
guydore10004 <guydore10004@gmail.com>
2022-05-25 09:05:44 +0300

    p3q1-15

---
+++ pandas3.py
@@ -0,0 +1,88 @@
+import pandas as pd
+
+#question 1
+
+df_people = pd.read_excel('people.xlsx')
+df_animals = pd.read_excel('animals.xlsx')
+
+df1 = df_people.rename(columns={'ID' : 'PersonID' , 'name' : 'PersonName' , 'age' : 'PersonAge'})
+
+
+
+#question 2
+
+df2 = df_animals.rename(columns={'ID' : 'AnimalID' , 'Name' : 'AnimalName' , 'Age' : 'AnimalAge'})
+
+
+#question 3
+
+
+df3 = df_people.merge(df_animals, left_on='ID' , right_on='Owner_ID')
+
+
+
+#question 4
+
+df4 = df_people.merge(df_animals, left_on='ID' , right_on='Owner_ID' , how='left')
+
+
+#question 5
+
+df5 = df3['Name_x'].unique()
+
+
+
+#question 6
+
+df6 = df3.loc[(df3['Name_x'] == 'Ido')]
+
+
+
+#question 7
+
+df7 = df3.loc[(df3['Age_x'] > 20)]
+
+
+#question 8
+
+df8 = df3.loc[(df3['Age_x'] >= 30)]
+
+
+#question 9
+
+df9 = df3.loc[(df3['Gender'] == 'F') & (df3['Type'] == 'dog') | (df3['Type'] == 'cat')]
+
+
+
+#question 10
+
+df10 = df4.loc[(df4['Gender'] == 'M') & (df4['Type'].isnull())]
+
+
+
+#question 11
+
+df11 = df3.loc[(df3['Gender'] == 'F') & (df3['Color'] == 'white')]
+
+
+#question 12
+
+df12 = df3.loc[(df3['Gender'] == 'M') & (df3['Type'] == 'dog') & (df3['Age_x'] >= 21)]
+df12 = df12['Color'].unique()
+
+
+#question 13
+
+df13 = df10['Name_x']
+
+
+#question 14
+
+df14 = df3.loc[(df3['Gender'] == 'F') & (df3['Color'] != 'black') & (df3['Age_y'] >= 3)]
+
+
+#question 15
+
+df15 = df3.loc[(df3['Name_y'] == df3['Name_y'])]
+df15 = df15.Name_y.value_counts()
+df15 = df15[df15 > 1]
